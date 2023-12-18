d826f59a7a561d30fe130862b1d45789d2428872
Wai-Shing Luk <luk036@gmail.com>
2023-12-18 20:05:30 +0800

    apply cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod csd;
 pub mod lcsre;
 
-pub use crate::csd::{to_csd, to_csd_i, to_decimal, to_csdfixed, to_decimal_i};
+pub use crate::csd::{to_csd, to_csd_i, to_csdfixed, to_decimal, to_decimal_i};
 pub use crate::lcsre::longest_repeated_substring;
 
 #[cfg(test)]
