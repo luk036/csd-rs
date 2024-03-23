4faa978ed6ba69067f8af9d9f05fea197ed889ce
Wai-Shing Luk <luk036@gmail.com>
2024-03-23 14:47:31 +0800

    apply cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ mod csd;
 mod lcsre;
 
 use crate::csd::{to_csd, to_csdfixed, to_decimal};
-use argparse::{ArgumentParser, StoreTrue, Store, Print};
+use argparse::{ArgumentParser, Print, Store, StoreTrue};
 
 fn main() {
     let mut verbose = false;
@@ -25,28 +25,33 @@ fn main() {
     let mut csdstr = String::new();
     let mut nnz = 3;
     let mut places = 4;
-    {  // this block limits scope of borrows by ap.refer() method
+    {
+        // this block limits scope of borrows by ap.refer() method
         let mut ap = ArgumentParser::new();
         ap.set_description("Canonical Signed Digit (CSD) Conversion.");
         ap.refer(&mut verbose)
-            .add_option(&["-v", "--verbose"], StoreTrue,
-            "Be verbose");
+            .add_option(&["-v", "--verbose"], StoreTrue, "Be verbose");
         ap.refer(&mut csdstr)
-            .add_option(&["-d", "--to_decimal"], Store,
-            "Convert to decimal");
-        ap.refer(&mut decimal)
-            .add_option(&["-c", "--to_csd"], Store,
-            "Convert to CSD with places (default is 4)");
+            .add_option(&["-d", "--to_decimal"], Store, "Convert to decimal");
+        ap.refer(&mut decimal).add_option(
+            &["-c", "--to_csd"],
+            Store,
+            "Convert to CSD with places (default is 4)",
+        );
         ap.refer(&mut places)
-            .add_option(&["-p", "--places"], Store,
-            "Specify the places");
+            .add_option(&["-p", "--places"], Store, "Specify the places");
         ap.refer(&mut nnz)
-            .add_option(&["-z", "--nnz"], Store,
-            "Specify the number of non-zeros");
-        ap.refer(&mut decimal2)
-            .add_option(&["-f", "--to_csdfixed"], Store,
-            "Convert to CSD with fixed number of non-zeros (default is 3)");
-        ap.add_option(&["-V", "--version"], Print(env!("CARGO_PKG_VERSION").to_string()), "Show version");
+            .add_option(&["-z", "--nnz"], Store, "Specify the number of non-zeros");
+        ap.refer(&mut decimal2).add_option(
+            &["-f", "--to_csdfixed"],
+            Store,
+            "Convert to CSD with fixed number of non-zeros (default is 3)",
+        );
+        ap.add_option(
+            &["-V", "--version"],
+            Print(env!("CARGO_PKG_VERSION").to_string()),
+            "Show version",
+        );
         ap.parse_args_or_exit();
     }
 
