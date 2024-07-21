9df16b2ac0fe1d55e374f521d0608207c437c873
Wai-Shing Luk <luk036@gmail.com>
2024-07-21 06:40:04 +0000

    apply cargo fmt

--- src/lcsre.rs
+++ src/lcsre.rs
@@ -50,7 +50,6 @@ pub fn longest_repeated_substring(sv: &str) -> String {
     }
 
     // Constructing the result substring if there's a non-empty result
-    
 
     if res_length > 0 {
         sv[index - res_length..index].to_string()
